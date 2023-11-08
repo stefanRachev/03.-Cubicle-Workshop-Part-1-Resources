74d6beee4ffc44e0bc805855862579e1996afe9d
stefanRachev <122920341+stefanRachev@users.noreply.github.com>
2023-11-08 21:01:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
+const express = require("express");
 
+const app = express();
+const PORT = 5050;
 
+app.get("/", (res, req) => {
+  res.send("hello from express server");
+});
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
