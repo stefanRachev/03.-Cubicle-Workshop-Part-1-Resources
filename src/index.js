7b13d9621d359f42b90c13987cc8844b4ff9d16a
stefanRachev <122920341+stefanRachev@users.noreply.github.com>
2023-11-09 16:49:25 +0200

    CUB-1 initialize handlebars setup first default view

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
 
 const app = express();
 const PORT = 5050;
 
-app.get("/", (res, req) => {
-  res.send("hello from express server");
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
