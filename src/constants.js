e18105f9d205f16e4449089221dae431f766bdac
stefanRachev <122920341+stefanRachev@users.noreply.github.com>
2023-11-15 07:47:45 +0200

    extract  connection string in the config,connect app to the db

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 const constants = {
   PORT: 5050,
+  URL: "mongodb://127.0.0.1:27017/cubical-sep-2023",
 };
 
 module.exports = constants;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+
 const handlebarsConfig = require("./config/handlebarsConfig");
 const expressConfig = require("./config/expressConfig");
+const dbConnect = require("./config/dbConfig");
+
 const { PORT } = require("./constants");
 const routes = require("./router");
 
@@ -9,6 +12,10 @@ const app = express();
 expressConfig(app);
 handlebarsConfig(app);
 
-app.use(routes)
+dbConnect()
+  .then(() => console.log("Successfully connected to the DB!"))
+  .catch((err) => console.log(`Error while connecting in DB: ${err}`));
+
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
